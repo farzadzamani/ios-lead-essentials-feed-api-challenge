b7de61bee21bafca3c396d238618ff5beb6a7e31
FARZAD ZAMANI <farzad.zamani@gmail.com>
2021-09-01 10:51:53 -0700

    - Add private FeedImageMapper class to map the JsonFeedImageModel (json payload model) to domain model (FeedImage) - Unwrap the success result, check for correct response and successfull json data mapping  if either of them failed pass the invalidDate error

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,13 +22,49 @@ public final class RemoteFeedLoader: FeedLoader {
 		self.client.get(from: url) { clientResult in
 
 			switch clientResult {
-			case .success((_, let response)):
-				if response.statusCode != 200 {
+			case .success((let data, let response)):
+				if response.statusCode == 200,
+				   let feedImages = try? FeedImageMapper.map(data) {
+					completion(.success(feedImages))
+				} else {
 					completion(.failure(Error.invalidData))
 				}
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
+		}
+	}
+}
+
+// MARK: - FeedImageMapper
+private final class FeedImageMapper {
+	private typealias Root = [JsonFeedImageModel]
+
+	// MARK: - JsonFeedImageModel
+	private struct JsonFeedImageModel: Codable {
+		let imageId: UUID
+		let imageDescription: String?
+		let imageLocation: String?
+		let imageURL: URL
+
+		enum CodingKeys: String, CodingKey {
+			case imageId = "image_id"
+			case imageDescription = "image_desc"
+			case imageLocation = "image_loc"
+			case imageURL = "image_url"
+		}
+
+		var feedImage: FeedImage {
+			return FeedImage(id: imageId, description: imageDescription, location: imageLocation, url: imageURL)
+		}
+	}
+
+	static func map(_ data: Data) throws -> [FeedImage] {
+		guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
+			throw RemoteFeedLoader.Error.invalidData
+		}
+		return root.map {
+			$0.feedImage
 		}
 	}
 }
